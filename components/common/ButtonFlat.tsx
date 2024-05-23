47129aa133320d4a198ee1fb2d4270f8054710b2
chibuzorEbede <chibuzorebede@gmail.com>
2024-05-23 04:44:08 -0500

    menu

--- components/common/ButtonFlat.tsx
+++ components/common/ButtonFlat.tsx
@@ -1,4 +1,4 @@
-const ButtonFlat = ({ text, w, h, c }) => {
+const ButtonFlat = ({ text, w, h, c = '' }) => {
   return (<button className={`px-2  ${c ? c : 'text-ag-gold'} text-center rounded-md text-lg font-normal capitalize `}>{text}</button>)
 }
 
